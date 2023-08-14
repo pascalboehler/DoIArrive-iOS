4e59a5ee52e3288ef02df2ea62518a94595733e8
Pascal Böhler <pascal@bembel.dev>
2023-08-14 08:41:34 +0200

    feat: Implemented Stack View

--- DoIArrive-iOS/Pages/HomeView.swift
+++ DoIArrive-iOS/Pages/HomeView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Hello world")
+            }
+            .navigationTitle("Home")
+        }
+        
     }
 }
 
