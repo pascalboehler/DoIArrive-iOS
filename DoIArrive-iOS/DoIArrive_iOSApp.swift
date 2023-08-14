06b99130906175f55e43b2fa0feba1276bbfd7e7
Pascal Böhler <pascal@bembel.dev>
2023-08-14 08:22:11 +0200

    Initial Commit

---
+++ DoIArrive-iOS/DoIArrive_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoIArrive_iOSApp.swift
+//  DoIArrive-iOS
+//
+//  Created by Pingu on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct DoIArrive_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
